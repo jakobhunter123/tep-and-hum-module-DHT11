758b2bc1e473bf4702ae88bc0bce3473cd7d752c
AV-35 <105905709+AV-35@users.noreply.github.com>
2022-05-29 20:48:00 +0200

    Paralelno

--- PPC/PPC.ino
+++ PPC/PPC.ino
@@ -1,14 +1,14 @@
 //Librarys
 #include <Arduino.h>
-#include "DHTesp.h"
-/*/#include "DHT.h"/*/
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "DHTesp.h"
+/*/#include "DHT.h"/*/
 
 //files
 #include "Icon.h"
 
-
+int x = 0;
 
 //DHT
 const int DHT_PIN = 15;
@@ -21,16 +21,15 @@ DHTesp dhtSensor;
 
 
 //display define
-#define SCREEN_WIDTH 128 // display display width, in pixels
-#define SCREEN_HEIGHT 64 // display display height, in pixels
+#define SCREEN_WIDTH 128 // Sirina
+#define SCREEN_HEIGHT 64 // Visina
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
+//definiranje default spremenjlivk v DisplayCenter
 void DisplayCenter(String text = "Test" ,int size = 2, String center = "C", String Symbol = "", int x = 0 , int y = 0);
 
 
@@ -45,12 +44,10 @@ int M = 14;
 //Spremenjlivke za prebrane vrednsoti
 float humi;
 float tempC;
-float tempF;
 
 //Spremenjlivke za povprecne vrednsoti
 float humi_Av;
 float tempC_Av;
-float tempF_Av;
 
 int  i = 0;
 
@@ -59,12 +56,21 @@ String H = "";
 String C = "";
 
 
+char Terminal [50];
+
+
 //Timer
 unsigned long starttime = 0;
 unsigned long endtime = 0;
 
+TaskHandle_t DisplayCore;
 
 void setup() {
+
+  //definira na katerme procesorju se izvaja Task
+  xTaskCreatePinnedToCore(DisplayCorecode,"Display",10000,NULL,1,&DisplayCore,0);                         
+  delay(500); 
+
 
 
   Serial.begin(9600);
@@ -74,20 +80,6 @@ void setup() {
   /*/
   HT.begin();
   /*/
-
-  //For display
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    while (true);
-  }
-  
-  display.clearDisplay();
-  display.setRotation(2);
-  display.drawBitmap(0, 0, WatherIcon, 128, 64, WHITE);
-  display.invertDisplay(true);
-  display.setTextColor(WHITE);
-  display.display();
-
   
   //LED 
   pinMode(Z, OUTPUT);
@@ -101,7 +93,6 @@ void loop() {
 
   humi_Av = 0;
   tempC_Av = 0;
-  tempF_Av = 0;
 
   i = 0;
 
@@ -109,70 +100,41 @@ void loop() {
   endtime = starttime;
 
   
-  
-  while ((endtime - starttime) <=1000) // do this loop for up to 1000mS
-  {
+
+  while ((endtime - starttime) <=5000) { // 5 selunden timer ki racuna povprecje temp/humi
     
     TempAndHumidity  data = dhtSensor.getTempAndHumidity();
 
-
-
     humi = (data.humidity);
     tempC = (data.temperature);
-    tempF = (data.temperature);
 
     /*/
     humi = HT.readHumidity();
     tempC = HT.readTemperature();
-    tempF = HT.readTemperature(true);
     /*/
    
-
     i += 1;
 
     humi_Av += humi;
     tempC_Av += tempC;
-    tempF_Av += tempF;
     delay(100);
-
 
     endtime = millis();
   }
 
 
+
   humi_Av  /= i;
   tempC_Av /= i;
-  tempF_Av /= i;
+
+  H = String(humi_Av);
+  C = String(tempC_Av);
+
+  sprintf(Terminal,"vlaga: %s % Temperatura: %s C ",H,C);
+  Serial.println(Terminal);
 
 
-  Serial.print("Vlaga:");
-  Serial.print(humi_Av,1);
-  Serial.print("%");
-  Serial.print(" Temperatura:");
-  Serial.print(tempC_Av,1);
-  Serial.print("C ~ ");
-  Serial.print(tempF_Av,1);
-  Serial.println("F");
-
-  display.invertDisplay(false);
-  display.clearDisplay();
-
-  H = String(humi_Av)/*/ + "%"/*/;
-  C = String(tempC_Av)/*/ + "%"/*/;
-
-  
-  DisplayCenter(C, 2, "C", "C", 0 , -10);
-  DisplayCenter("Temp", 2, "CT");
-
-
-  delay(2500);
-  display.clearDisplay();
-
-  DisplayCenter(H, 2, "C", "%", 0 , -10);
-  DisplayCenter("Humidity", 2, "CT");
-
-  display.display(); 
-
+  //priziga LED-ice glede na temperaturo
   if (tempC_Av >= 30){
     digitalWrite(R,HIGH);
   }
@@ -185,7 +147,43 @@ void loop() {
 
 }
 
+void DisplayCorecode( void * parameter ){ // isto kot void setup samo da deluje na procesorju 0
+  Serial.begin(9600);
+  Serial.print("core ");
+  Serial.println(xPortGetCoreID());
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    while (true);
+  }
 
+  //narise Icono na zagono
+  display.clearDisplay();
+  display.setRotation(2);
+  display.drawBitmap(0, 0, WatherIcon, 128, 64, WHITE);
+  display.invertDisplay(true);
+  display.display();
+
+  delay(5000);
+  display.invertDisplay(false);
+
+
+  for(;;){    // isto kot void loop samo da deluje na procesorju 0  
+
+      display.clearDisplay();
+      DisplayCenter(C, 2, "C", "C", 0 , -10);
+      DisplayCenter("Temp", 2, "CT");
+
+
+      delay(2500);
+      display.clearDisplay();
+      DisplayCenter(H, 2, "C", "%", 0 , -10);
+      DisplayCenter("Humidity", 2, "CT");
+      delay(2500);
+
+     
+       
+    }
+  } 
 
 
 
@@ -202,19 +200,17 @@ void DisplayCenter(String text ,int size, String center, String Symbol, int x , 
   uint16_t Swidth;
   uint16_t Sheight;
 
-
-
+  display.setTextColor(WHITE);
   display.setTextSize(size);
-
+  //zabelezi veliksot symbola
   display.getTextBounds(text, 0, 0, &x1, &y1, &width, &height);
   display.getTextBounds(Symbol, 0, 0, &Sx1, &Sy1, &Swidth, &Sheight);
 
 
-
+  
   if (Symbol == "C"){
     if (center == "C"){//Center
       display.setCursor((SCREEN_WIDTH - width - Swidth-5) / 2 +  x , (SCREEN_HEIGHT -  height) / 2 -y);
-      Serial.println("TestC0");
     }
     else if(center == "CT"){//Spodnji center
       display.setCursor((SCREEN_WIDTH - width - Swidth-5) / 2 +  x, (SCREEN_HEIGHT - height) / 128 - y);
@@ -223,8 +219,9 @@ void DisplayCenter(String text ,int size, String center, String Symbol, int x , 
       display.setCursor((SCREEN_WIDTH - width - Swidth-5) / 2 +  x, (SCREEN_HEIGHT - height) / 1 - y);
     }
 
+
     display.println(text);
-    Serial.println("TestC1");
+    //izrise znak za stopinje odvisno od velikosti teksta
     display.drawCircle((SCREEN_WIDTH + width - Swidth - 5) / 2 + 2 + x , (SCREEN_HEIGHT -  height) / 2 +2 - y , size, WHITE);  // print degree symbols (  )
     display.setCursor((SCREEN_WIDTH + width - Swidth) / 2 + (2+size) + x,(SCREEN_HEIGHT - height) / 2 - y );
 
@@ -232,7 +229,6 @@ void DisplayCenter(String text ,int size, String center, String Symbol, int x , 
   else {
 
      if (Symbol == ""){
-          Serial.println(Swidth);
       Swidth  -= Swidth;
     }
 
@@ -244,12 +240,9 @@ void DisplayCenter(String text ,int size, String center, String Symbol, int x , 
     }
     else if(center == "CB"){//zgornji senter
       display.setCursor((SCREEN_WIDTH - width - Swidth ) /2 +  x, (SCREEN_HEIGHT - height) / 1 - y);
-
     }
-  
-    display.print(text);
-    Serial.println(Swidth);
 
+    display.print(text);
 
   }  
 
@@ -259,3 +252,6 @@ void DisplayCenter(String text ,int size, String center, String Symbol, int x , 
 }
   
   
+
+
+
